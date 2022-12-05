0170f8a10bb4ded57047b795d9ade047c913a18e
Eji1700 <regulus87@yahoo.com>
2022-12-04 17:46:22 -0800

    Initial solution, however 99% positive it would fail if one of the 3 elves was in the very first group

--- Day1/Calories.fsx
+++ Day1/Calories.fsx
@@ -1,4 +1,39 @@
+// Code (or parts of) blatantly stolen from https://github.com/Eliemer/AdventOfCode2022
+// I spend waaay to long on simple tasks and often overcomplicate them so I'm walking 
+// through this better repo to better understand their much more elegant solution. 
+// Even my reading of the input.txt was more of a mess, let alone my proposed split solution.
+open System
 open System.IO
 
-let inputData =  File.ReadLines "input.txt"
-File.WriteAllLines(Directory.GetCurrentDirectory() + "/test.txt", inputData)
+let inputData =  File.ReadLines @".\Day1\input.txt"
+
+let splitBy (cond: 'a -> bool) (source: 'a seq) =
+    let mutable i = 0
+
+    source 
+    |> Seq.groupBy(fun x ->
+        if cond x then i <- i + 1
+        i
+    )
+    |> Seq.map(fun entry -> snd entry)
+
+let elfs = 
+    inputData
+    |> splitBy String.IsNullOrWhiteSpace
+    |> Seq.map
+        (Seq.tail 
+        >> (Seq.map Int32.Parse)
+        >> Seq.sum)
+    |> Seq.cache
+
+let Answer1 =
+    elfs
+    |> Seq.max
+
+let Answer2 =
+    elfs
+    |> Seq.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
+
+Answer2
