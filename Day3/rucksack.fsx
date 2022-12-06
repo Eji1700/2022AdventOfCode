3de5f5a6591ac3a48f080894f5881420d8efaa3c
Eji1700 <regulus87@yahoo.com>
2022-12-05 19:55:49 -0800

    the fools way to convert char to int i'm sure

--- Day3/rucksack.fsx
+++ Day3/rucksack.fsx
@@ -1,3 +1,13 @@
 open System.IO
 
-let data = File.ReadAllLines @".\Day3\input.txt"
+// let data = File.ReadAllLines @".\Day3\input.txt"
+
+let charToPriority (c:char) =
+    let charInt = int c
+    if charInt >= 65 && charInt <= 90 then
+        charInt - 38
+    elif charInt >= 97 && charInt <= 122 then 
+        charInt - 96
+    else
+        charInt
+charToPriority 'z'
