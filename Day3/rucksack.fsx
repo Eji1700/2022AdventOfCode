b9491cd1ae2a9085dd48e0544a6065cce35143be
Eji1700 <regulus87@yahoo.com>
2022-12-05 20:45:30 -0800

    Got answer 2, don't love how i did it

--- Day3/rucksack.fsx
+++ Day3/rucksack.fsx
@@ -15,7 +15,7 @@ let Answer1 =
     data
     |> Seq.map(fun ruckSacks ->
         ruckSacks
-        |> Seq.map charToPriority
+        |> Seq.map charToPriority // Putting the maps next to each other means i could reduce this to map(x >> y) but don't want to bother right now.
         |> Seq.splitInto 2
         |> Seq.map(fun intSack -> intSack |> Set.ofArray ) // Feel like maybe there's a Seq.function that does this cleaner but I couldn't find it?
         |> Set.intersectMany
@@ -23,4 +23,17 @@ let Answer1 =
     )
     |> Seq.sum
 
+let Answer2 = // Got this one pretty quickly but I hate it?  Again i could probably find a way to reduce the double map to map(x>>y), but even then I feel like this is...obtuse?
+    data
+    |> Seq.map(fun ruckSacks ->
+        ruckSacks
+        |> Seq.map charToPriority
+    )
+    |> Seq.chunkBySize 3
+    |> Seq.map(fun groupSacks ->
+        groupSacks |> Array.map Set.ofSeq |> Set.intersectMany |> Set.maxElement
+    )
+    |> Seq.sum
+
 Answer1
+Answer2
