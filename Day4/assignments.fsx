f785283c5112b798710592d2d36d732896f783e8
Eji1700 <regulus87@yahoo.com>
2022-12-06 20:52:44 -0800

    Spent waaaaay too much time trying to hardcode all the ifs and getting it wrong.  Just making isBetween solved it instantly.

--- Day4/assignments.fsx
+++ Day4/assignments.fsx
@@ -7,6 +7,9 @@ let getStartandEnd (assignment: string) =
     let arr = assignment.Split("-")
     int arr[0],  int arr[1]
 
+let isBetween a b c =
+    b >= a && b <= c 
+
 let findFullyContainedRange (elf1Start, elf1End) (elf2Start, elf2End) =
     if elf1Start <= elf2Start && elf1End >= elf2End then
         Some 1
@@ -14,6 +17,14 @@ let findFullyContainedRange (elf1Start, elf1End) (elf2Start, elf2End) =
         Some 1
     else
         None 
+
+let findRangeOverlap (elf1Start, elf1End) (elf2Start, elf2End) =
+    if isBetween elf1Start elf2Start elf1End then 
+        Some 1
+     elif isBetween elf2Start elf1Start elf2End then 
+        Some 1
+    else 
+        None
 
 let Answer1 =
     inputData
@@ -25,4 +36,16 @@ let Answer1 =
     )
     |> Array.length
 
+let Answer2 = 
+    inputData
+    |> Array.choose(fun pair ->
+        let arr = pair.Split(",")
+        let elf1 = getStartandEnd arr[0]
+        let elf2 = getStartandEnd arr[1]
+        findRangeOverlap elf1 elf2   
+    )
+    |> Array.length
+
+
 Answer1
+Answer2
