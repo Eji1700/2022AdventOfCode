8fa2c5739c0c16ead55d257764a3aa11747132f9
Eji1700 <regulus87@yahoo.com>
2022-12-17 00:36:14 -0800

    takewhile became array.where

--- Day5/craneGame.fsx
+++ Day5/craneGame.fsx
@@ -4,5 +4,4 @@ open System.IO
 let inputData = File.ReadAllLines @".\Day5\input.txt"
 
 let board = inputData |> Array.take 9
-// let instuctions = inputData |> Array.takeWhile(fun entry -> entry.StartsWith "move")  Why doesn't this work?
-let instuctions = inputData |> Array.splitAt 10 |> snd
+let instuctions = inputData |> Array.where(fun entry -> entry.StartsWith "move")
