f678da0cd5f46de4629dc15b826e504a57bf86fc
Eji1700 <regulus87@yahoo.com>
2022-12-17 00:43:27 -0800

    Feels like a strong start.  Have the board split already

--- Day5/craneGame.fsx
+++ Day5/craneGame.fsx
@@ -3,5 +3,12 @@ open System.IO
 
 let inputData = File.ReadAllLines @".\Day5\input.txt"
 
-let board = inputData |> Array.take 9
+let board = inputData |> Array.take 8
 let instuctions = inputData |> Array.where(fun entry -> entry.StartsWith "move")
+let boardParse =
+    board
+    |> Array.map( fun row ->
+        row 
+        |> Seq.splitInto 9
+    )
+boardParse
