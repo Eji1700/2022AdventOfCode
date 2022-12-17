b7cae2af8a83ecf9c8154c32d292c8b951495e3e
Eji1700 <regulus87@yahoo.com>
2022-12-17 00:55:29 -0800

    Cleaner parsing

--- Day5/craneGame.fsx
+++ Day5/craneGame.fsx
@@ -5,10 +5,17 @@ let inputData = File.ReadAllLines @".\Day5\input.txt"
 
 let board = inputData |> Array.take 8
 let instuctions = inputData |> Array.where(fun entry -> entry.StartsWith "move")
+
+let cellParse (cell: char[]) = 
+    if cell[0] = '[' then cell[1].ToString()
+    else ""
+
 let boardParse =
     board
     |> Array.map( fun row ->
         row 
         |> Seq.splitInto 9
+        |> Seq.map cellParse 
+        |> Seq.toArray
     )
 boardParse
