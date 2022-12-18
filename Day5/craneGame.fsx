6e75d81028ad1397338548c9b32bf8f252fce40e
Eji1700 <regulus87@yahoo.com>
2022-12-17 21:06:49 -0800

    Rearranged Board stuff to have only valid values

--- Day5/craneGame.fsx
+++ Day5/craneGame.fsx
@@ -3,23 +3,8 @@ open System.IO
 
 let inputData = File.ReadAllLines @".\Day5\input.txt"
 
-let board = inputData |> Array.take 8
 let instuctions = inputData |> Array.where(fun entry -> entry.StartsWith "move")
-
-let cellParse (cell: char[]) = 
-    if cell[0] = '[' then cell[1].ToString()
-    else " "
-
-let boardParse boardArr =
-    boardArr
-    |> Array.map( fun row ->
-        row 
-        |> Seq.splitInto 9
-        |> Seq.map cellParse 
-        |> Seq.toArray
-    ) 
-    |> Array.transpose
-boardParse board
+let board = inputData |> Array.take 8
 
 type Instruction =
     {   Qty: int
@@ -44,3 +29,20 @@ module Instruction =
         )
 
 Instruction.Parse instuctions
+
+let cellParse (cell: char[]) = 
+    if cell[0] = '[' then cell[1].ToString() |> Some
+    else None
+
+let boardParse boardArr =
+    boardArr
+    |> Array.map( fun row ->
+        row 
+        |> Seq.splitInto 9
+        |> Seq.toList ) 
+    |> List.transpose
+    |> List.map(fun lst ->
+        lst
+        |> List.choose cellParse
+    )
+boardParse board
